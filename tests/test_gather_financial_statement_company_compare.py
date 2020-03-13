958e6e79e0f8cd8e7938a30afeca884cc7c5af84
sirine_chahma <sirine.chahma@supelec.fr>
2020-03-13 01:28:10 -0700

    fixing PEP8 issues

--- tests/test_gather_financial_statement_company_compare.py
+++ tests/test_gather_financial_statement_company_compare.py
@@ -5,7 +5,7 @@ from pyntrinio.pyntrinio import gather_financial_statement_company_compare
 from pytest import raises
 import pandas as pd
 """
-This script tests the gather_financial_statement_company_compare functions 
+This script tests the gather_financial_statement_company_compare functions
 in the pyntrinio module.
 """
 
@@ -28,9 +28,11 @@ def test_type_input():
         gather_financial_statement_company_compare(
             la, ticker, statement, year, period)
     with raises(TypeError):
-        gather_financial_statement_company_compare(123, ticker, statement, year, period)
+        gather_financial_statement_company_compare(
+            123, ticker, statement, year, period)
     with raises(TypeError):
-        gather_financial_statement_company_compare(123.3, ticker, statement, year, period)
+        gather_financial_statement_company_compare(
+            123.3, ticker, statement, year, period)
 
     # Check that when the statement is not a string, it returns an error
     with raises(TypeError):
@@ -58,7 +60,7 @@ def test_type_input():
             api_key, ticker, statement, years, period)
     with raises(TypeError):
         gather_financial_statement_company_compare(
-        api_key, ticker, statement, ['123'], period)
+            api_key, ticker, statement, ['123'], period)
 
     # Check that when the period is not a string, it returns an error
     with raises(TypeError):
@@ -118,7 +120,7 @@ def test_type_input():
 def test_output():
     '''
     Tests if the output seems right (type, dimension and one value)
-    ''' 
+    '''
     api_key = 'OmEzNGY3MGEwMDIwZGM5Y2UxNDZhNzUzMTgzYTJiNWI2'
     ticker = ['AAPL', 'CSCO', 'CAT', 'AXP', 'IBM']
     statement = 'income_statement'
@@ -145,11 +147,11 @@ def test_output():
 def test_year():
     '''
     Tests if the length of year is 4
-    ''' 
+    '''
     api_key = 'OmEzNGY3MGEwMDIwZGM5Y2UxNDZhNzUzMTgzYTJiNWI2'
     ticker = ['AAPL', 'CSCO']
     statement = 'income_statement'
-    period = 'Q1' 
+    period = 'Q1'
 
     # Check that when the year is not of length 4, it returns an error
     with raises(Exception):
