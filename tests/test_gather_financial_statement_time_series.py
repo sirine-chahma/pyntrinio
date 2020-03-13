ca270a2b183be284856b1b236971596611c5cbdd
hatchi28 <aakanksha023@gmail.com>
2020-03-13 13:05:04 -0700

    all_build_issues_resolved

--- tests/test_gather_financial_statement_time_series.py
+++ tests/test_gather_financial_statement_time_series.py
@@ -13,20 +13,11 @@ ticker = 'CVX'
 statement = 'income_statement'
 year = ['2017', '2018']
 period = ['Q1', 'Q3']
-var_1 = OjQ0Yz
-var_2 = CVX
-var_3 = years
-var_4 = income_statement
-var_5 = Q1
-
 
 def test_api_key_format():
     """
     Test if the api_key is a string
     """
-    with raises(NameError):
-        gather_financial_statement_time_series(
-            var_1, ticker, statement, year, period)
     with raises(TypeError):
         gather_financial_statement_time_series(
             123, ticker, statement, year, period)
@@ -39,9 +30,6 @@ def test_ticker_format():
     """
     Test if the ticker is a string
     """
-    with raises(NameError):
-        gather_financial_statement_time_series(
-            api_key, var_2, statement, year, period)
     with raises(TypeError):
         gather_financial_statement_time_series(
             api_key, 123, statement, year, period)
@@ -84,10 +72,6 @@ def test_year_format():
         gather_financial_statement_time_series(
             api_key, ticker, statement, 2017.0, period)
 
-    with raises(NameError):
-        gather_financial_statement_time_series(
-            api_key, ticker, statement, var_3, period)
-
     with raises(Exception):
         gather_financial_statement_time_series(
             api_key, ticker, statement, ['2017' '2018'], period)
@@ -98,10 +82,6 @@ def test_statement_format():
     Test if the statement is a string and
     available in a list of valid statement options
     """
-    with raises(NameError):
-        gather_financial_statement_time_series(
-            api_key, ticker, var_4, year, period)
-
     with raises(Exception):
         gather_financial_statement_time_series(
             api_key, ticker, 'income_statemen', year, period)
@@ -118,10 +98,6 @@ def test_period_format():
     with raises(Exception):
         gather_financial_statement_time_series(
             api_key, ticker, 'income_statement', year, 'Q1')
-
-    with raises(NameError):
-        gather_financial_statement_time_series(
-            api_key, ticker, 'income_statement', year, var_5)
 
 
 def test_final_format():
